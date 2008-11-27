e84208b7e4ccb414c423d0b2ed2127b7e8bc2831
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2008-11-27 12:47:40 +0000

    initial commit of a test bundle for the HTTP bundle

---
+++ platform/bundles/org.eclipse.gyrex.http.tests/src/org/eclipse/gyrex/http/tests/Activator.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2008 AGETO Service GmbH and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cloudfree.http.tests;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(final BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(final BundleContext context) throws Exception {
+	}
+
+}
