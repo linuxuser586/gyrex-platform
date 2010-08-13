0139a7609ca9cf65e6ec216b9ddd5a575076ec28
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-08-13 16:09:09 +0000

    updated copyright headers

--- platform/bundles/org.eclipse.gyrex.http/src/org/eclipse/gyrex/http/application/IApplicationConstants.java
+++ platform/bundles/org.eclipse.gyrex.http/src/org/eclipse/gyrex/http/application/IApplicationConstants.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008 AGETO Service GmbH and others.
+ * Copyright (c) 2008, 2009 AGETO Service GmbH and others.
  * All rights reserved.
  *
  * This program and the accompanying materials are made available under the terms of the
