82b1256e8a83a12d91df1178222b62302f14ea45
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-09-03 08:49:38 +0000

    made public for testing

--- platform/bundles/org.eclipse.gyrex.persistence/src/org/eclipse/gyrex/persistence/internal/storage/RepositoryPreferences.java
+++ platform/bundles/org.eclipse.gyrex.persistence/src/org/eclipse/gyrex/persistence/internal/storage/RepositoryPreferences.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2009 AGETO Service GmbH and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
@@ -13,15 +13,18 @@ package org.eclipse.gyrex.persistence.internal.storage;
 
 import java.io.IOException;
 
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.equinox.security.storage.ISecurePreferences;
+
 import org.eclipse.gyrex.persistence.storage.settings.IRepositoryPreferences;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
 import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * Repository preferences implementation.
  */
-final class RepositoryPreferences implements IRepositoryPreferences {
+public final class RepositoryPreferences implements IRepositoryPreferences {
 
 	/** securePreferences */
 	private final ISecurePreferences securePreferences;
@@ -34,7 +37,7 @@ final class RepositoryPreferences implements IRepositoryPreferences {
 	 * @param securePreferences
 	 * @param eclipsePreferences
 	 */
-	RepositoryPreferences(final ISecurePreferences securePreferences, final IEclipsePreferences eclipsePreferences) {
+	public RepositoryPreferences(final ISecurePreferences securePreferences, final IEclipsePreferences eclipsePreferences) {
 		this.securePreferences = securePreferences;
 		this.eclipsePreferences = eclipsePreferences;
 	}
