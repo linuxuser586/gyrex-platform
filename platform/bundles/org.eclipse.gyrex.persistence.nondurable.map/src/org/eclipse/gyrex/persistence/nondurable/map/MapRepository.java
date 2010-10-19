07c03266483427ca1438639958f6a571ccbf3a98
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-10-19 07:39:44 +0000

    Javadoc fix

--- platform/bundles/org.eclipse.gyrex.persistence.nondurable.map/src/org/eclipse/gyrex/persistence/nondurable/map/MapRepository.java
+++ platform/bundles/org.eclipse.gyrex.persistence.nondurable.map/src/org/eclipse/gyrex/persistence/nondurable/map/MapRepository.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009 AGETO Service GmbH and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
@@ -22,7 +22,7 @@ import org.eclipse.gyrex.persistence.storage.provider.RepositoryProvider;
  * <p>
  * The repository may be used for short living objects. It does not offer any
  * transaction or other rich persistence capabilities. It's backed by a
- * {@link ConcurrentNavigableMap} to simple store objects based on keys.
+ * {@link ConcurrentNavigableMap} to simply store objects based on keys.
  * </p>
  * <p>
  * This class may be subclassed by clients that want to contribute a custom map
