612cc25c39299537d23d0246aef6354d543725ed
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2011-02-26 08:48:54 +0000

    initial commit of new test bundle

---
+++ platform/bundles/org.eclipse.gyrex.server.tests/src/org/eclipse/gyrex/server/tests/internal/AllServerTests.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2011 Gunnar Wagenknecht and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ */
+package org.eclipse.gyrex.server.tests.internal;
+
+import org.eclipse.gyrex.cloud.tests.internal.AllCloudTests;
+import org.eclipse.gyrex.context.tests.internal.AllContextTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ AllCloudTests.class, AllContextTests.class })
+public class AllServerTests {
+
+}
