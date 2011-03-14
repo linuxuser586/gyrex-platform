321c4b7bed8247f359112aadc2e9f31313891721
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2011-03-14 07:55:52 +0000

    wait for node approval

--- platform/bundles/org.eclipse.gyrex.boot.tests/src/org/eclipse/gyrex/boot/tests/internal/ServerTestApplication.java
+++ platform/bundles/org.eclipse.gyrex.boot.tests/src/org/eclipse/gyrex/boot/tests/internal/ServerTestApplication.java
@@ -11,10 +11,16 @@
  *******************************************************************************/
 package org.eclipse.gyrex.boot.tests.internal;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.equinox.app.IApplication;
 
 import org.eclipse.gyrex.boot.internal.app.ServerApplication;
+import org.eclipse.gyrex.cloud.internal.CloudState;
+import org.eclipse.gyrex.cloud.internal.NodeInfo;
 import org.eclipse.gyrex.cloud.internal.zk.ZooKeeperGate;
+import org.eclipse.gyrex.cloud.internal.zk.ZooKeeperGate.IConnectionMonitor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -44,33 +50,53 @@ public class ServerTestApplication extends ServerApplication implements IApplica
 						System.setProperty("gyrex.preferences.instancebased", Boolean.FALSE.toString());
 					}
 
-					// wait for cloud connection
-					long timeout = Long.getLong("gyrex.servertestapp.timeout", 60000l);
-					ZooKeeperGate gate = null;
-					while ((timeout > 0) && (gate == null)) {
-						try {
-							gate = ZooKeeperGate.get();
-						} catch (final IllegalStateException e) {
-							LOG.info("waiting for ZooKeeper connection...");
-							timeout -= SLEEP_TIME;
-							Thread.sleep(SLEEP_TIME);
+					// prepare wait for cloud connection
+					final long timeout = Long.getLong("gyrex.servertestapp.timeout", 60000l);
+					final CountDownLatch connectedSignal = new CountDownLatch(1);
+					ZooKeeperGate.addConnectionMonitor(new IConnectionMonitor() {
+						@Override
+						public void connected(final ZooKeeperGate gate) {
+							connectedSignal.countDown();
 						}
-					}
 
-					// final test
-					if (gate == null) {
+						@Override
+						public void disconnected(final ZooKeeperGate gate) {
+							LOG.warn("ZooKeeper disconnected! No more tests should be run now.");
+						}
+					});
+
+					// now wait for cloud connection
+					LOG.info("waiting for ZooKeeper connection...");
+					if (!connectedSignal.await(timeout, TimeUnit.MILLISECONDS)) {
+						LOG.error("Timeout waiting for ZooKeeper connection.");
 						ServerApplication.signalShutdown(new Exception("Timeout while waiting for ZooKeeper connection. Unable to initialize cloud environment. Test execution aborted!"));
 						return Status.CANCEL_STATUS;
 					}
 
-					// TODO: implement optional wait for node approval (maybe using server role?)
+					//  wait for node approval
+					long approvalTimeout = 5000;
+					NodeInfo nodeInfo = CloudState.getNodeInfo();
+					while (((nodeInfo == null) || !nodeInfo.isApproved()) && (approvalTimeout > 0)) {
+						LOG.info("Waiting for automatic approval of node...");
+						approvalTimeout -= SLEEP_TIME;
+						Thread.sleep(SLEEP_TIME);
+						nodeInfo = CloudState.getNodeInfo();
+					}
+					if ((nodeInfo == null) || !nodeInfo.isApproved()) {
+						LOG.error("Timeout waiting for automatic node approval.");
+						ServerApplication.signalShutdown(new Exception("Timeout while waiting for automatic node approval. Unable to initialize cloud environment. Test execution aborted!"));
+						return Status.CANCEL_STATUS;
+					}
 
 					// execute tests
+					LOG.info("Executing tests...");
 					RemotePluginTestRunner.main(arguments);
+					LOG.info("Finished executing tests.");
 
 					// shutdown
 					ServerApplication.signalShutdown(null);
 				} catch (final Exception e) {
+					LOG.error("Failed executing tests. Signaling shutdown!", e);
 					ServerApplication.signalShutdown(e);
 				}
 				return Status.OK_STATUS;
@@ -78,5 +104,4 @@ public class ServerTestApplication extends ServerApplication implements IApplica
 		};
 		testRunner.schedule();
 	}
-
 }
