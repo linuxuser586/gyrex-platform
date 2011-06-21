7580dc2c112e76d026c956f2c6f06906fd4521eb
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2011-06-21 11:02:49 +0000

    ported to ARGS4J based commands

---
+++ platform/bundles/org.eclipse.gyrex.cloud/src/org/eclipse/gyrex/cloud/internal/console/CloudConsoleCommands.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 AGETO Service GmbH and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gyrex.cloud.internal.console;
+
+import org.eclipse.gyrex.cloud.internal.CloudDebug;
+import org.eclipse.gyrex.common.console.BaseCommandProvider;
+
+import org.eclipse.osgi.framework.console.CommandInterpreter;
+
+public class CloudConsoleCommands extends BaseCommandProvider {
+
+	/**
+	 * Creates a new instance.
+	 */
+	public CloudConsoleCommands() {
+		registerCommand("ls", ListNodesCmd.class);
+		registerCommand("approve", ApproveNodeCmd.class);
+		registerCommand("retire", RetireNodeCmd.class);
+		registerCommand("setConnectString", SetConnectStringCmd.class);
+		registerCommand("setTags", SetNodeTagsCmd.class);
+		registerCommand("setOffline", SetOfflineCmd.class);
+		registerCommand("setOnline", SetOnlineCmd.class);
+	}
+
+	public void _cloud(final CommandInterpreter ci) {
+		printStackTraces = CloudDebug.debug;
+		execute(ci);
+	}
+
+	@Override
+	protected String getCommandName() {
+		return "cloud";
+	}
+
+}
---
+++ platform/bundles/org.eclipse.gyrex.cloud/src/org/eclipse/gyrex/cloud/internal/console/SetOfflineCmd.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011 <enter-company-name-here> and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     <enter-developer-name-here> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gyrex.cloud.internal.console;
+
+import org.eclipse.gyrex.cloud.internal.CloudState;
+import org.eclipse.gyrex.common.console.Command;
+
+public class SetOfflineCmd extends Command {
+
+	/**
+	 * Creates a new instance.
+	 */
+	public SetOfflineCmd() {
+		super("sets the current node offline");
+	}
+
+	@Override
+	protected void doExecute() throws Exception {
+		CloudState.unregisterNode();
+		printf("Node un-registered from cloud!");
+	}
+}
