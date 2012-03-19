d4f32e0b3d7ff462966bb04f549454f25381b8ee
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-03-19 23:35:14 +0100

    use more sane default
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- bundles/org.eclipse.gyrex.logback.config/src/org/eclipse/gyrex/logback/config/internal/LogbackConfigApplication.java
+++ bundles/org.eclipse.gyrex.logback.config/src/org/eclipse/gyrex/logback/config/internal/LogbackConfigApplication.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2012 AGETO Service GmbH and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
  *
@@ -77,7 +77,7 @@ public class LogbackConfigApplication extends BaseApplication implements IApplic
 	}
 
 	private long getLastModified() {
-		return CloudScope.INSTANCE.getNode(LogbackConfigActivator.SYMBOLIC_NAME).getLong(PREF_LAST_MODIFIED, System.currentTimeMillis());
+		return CloudScope.INSTANCE.getNode(LogbackConfigActivator.SYMBOLIC_NAME).getLong(PREF_LAST_MODIFIED, 0);
 	}
 
 	@Override
