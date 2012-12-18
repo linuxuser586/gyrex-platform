5cec286f2ceace303b5fd5dee2fa17e433e1ea40
Andreas Mihm <andreas.mihm@ageto.net>
2012-12-18 09:30:23 +0100

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=396807 Provide an Gyrex appilcation which serves static content:
    - The goal is to be able to build a bundle containing a html5 frontend
    for an api based gyrex application. Both applications (html5 frontend
    and rest api app) then can be served from the same gyrex cluster making
    good use of the clustering technologies of gyrex
    
    - the static content shall be served out of a bundle folder OR in
    developmenbt mode it shall be served from a OS folder presented in an
    environment variable
    
    - The approach for the implementation is to implement application and
    application provider in org.eclipse.gyrex.http bundle and to use them as
    DS definitions in the bundle containing the static content

---
+++ bundles/org.eclipse.gyrex.http/src/org/eclipse/gyrex/http/internal/staticresource/StaticResourceApplication.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2012 AGETO Service GmbH and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Andreas Mihm - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gyrex.http.internal.staticresource;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+
+import org.eclipse.gyrex.context.IRuntimeContext;
+import org.eclipse.gyrex.http.application.Application;
+import org.eclipse.gyrex.http.application.context.IResourceProvider;
+import org.eclipse.gyrex.server.Platform;
+
+import org.eclipse.core.runtime.Path;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * a gyrex application, which serves static content (html, js, css). This
+ * application shall be used for creating html5 frontends in gyrex
+ * <p>
+ * the actual application instance is created as a declarative service
+ * declaration in a bundle, which holds the static content. The required service
+ * property resource.path needs to contain the folder inside the bundle e.g.
+ * "/web", which holds the static content.
+ * <p>
+ * There is also another option available in Gyrex Dev Mode Only (see
+ * {@link Platform}) of serving static content just from any other directory by
+ * setting an environment varaiable containing the fully qualified directory
+ * path and set the name of this environment variable as the DS property
+ * resource.devModeDocRootEnvVar. The goal of this option is to make the html
+ * development process easier.
+ */
+public class StaticResourceApplication extends Application {
+
+	private final Bundle bundle;
+	private final String bundleResourcePath;
+	private final String devModeDocRootEnvVar;
+
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param applicationId
+	 * @param context
+	 * @param bundle
+	 * @param bundleResourcePath
+	 *            the bundle resource path which holds the static content
+	 * @param devModeDocRootEnvVar
+	 *            the name of the environment variable, which holds the path to
+	 *            static content (only used in dev mode {@link Platform}) this
+	 *            setting overrides a bundleresource path
+	 */
+	public StaticResourceApplication(final String applicationId, final IRuntimeContext context, final Bundle bundle, final String bundleResourcePath, final String devModeDocRootEnvVar) {
+		super(applicationId, context);
+		this.bundle = bundle;
+		this.bundleResourcePath = bundleResourcePath;
+		this.devModeDocRootEnvVar = devModeDocRootEnvVar;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.gyrex.http.application.Application#doInit()
+	 */
+	@Override
+	protected void doInit() throws IllegalStateException, Exception {
+		getApplicationContext().registerResources("/", "", new IResourceProvider() {
+
+			@Override
+			public URL getResource(final String path) throws MalformedURLException {
+				if (Platform.inDevelopmentMode() && (devModeDocRootEnvVar != null) && (System.getenv(devModeDocRootEnvVar) != null)) {
+					final File file = new File(System.getenv(devModeDocRootEnvVar), path);
+					if (file.exists())
+						return file.toURI().toURL();
+					return null;
+				}
+				return bundle.getEntry(new Path(bundleResourcePath).append(path).toString());
+			}
+
+			@Override
+			public Set<String> getResourcePaths(final String path) {
+				// this method is not called through jetty integration in gyrex 
+				return null;
+			}
+		});
+	}
+}
---
+++ bundles/org.eclipse.gyrex.http/src/org/eclipse/gyrex/http/internal/staticresource/StaticResourceApplicationProvider.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2012 AGETO Service GmbH and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Andreas Mihm - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gyrex.http.internal.staticresource;
+
+import org.eclipse.gyrex.context.IRuntimeContext;
+import org.eclipse.gyrex.http.application.Application;
+import org.eclipse.gyrex.http.application.provider.ApplicationProvider;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentConstants;
+import org.osgi.service.component.ComponentContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * the ApplicationProvider for the {@link StaticResourceApplication}
+ */
+public class StaticResourceApplicationProvider extends ApplicationProvider {
+
+	private static final Logger LOG = LoggerFactory.getLogger(StaticResourceApplicationProvider.class);
+
+	/**
+	 * A component property for a component configuration that contains the
+	 * {@link ApplicationProvider#getId() application provider id} which shall
+	 * be used for registering the application provider. The value of this
+	 * property must be of type {@code String}. If no property is provided, a
+	 * fallback will be attempted to the
+	 * {@link ComponentConstants#COMPONENT_NAME} property.
+	 */
+	public static final String APPLICATION_PROVIDER_ID = "applicationProviderId";
+
+	private BundleContext bundleContext;
+
+	private String bundleResourcePath;
+	private String devModeDocRootEnvVar;
+
+	public void activate(final ComponentContext context) {
+		LOG.debug("StaticResourceApplicationProvider activation triggered for component '{}' (bundle {})", context.getProperties().get(ComponentConstants.COMPONENT_NAME), context.getBundleContext().getBundle());
+
+		// initialize application id
+		final String applicationProviderId = getApplicationProviderId(context);
+		try {
+			LOG.debug("Using application provider id '{}' for component '{}' (bundle {})", new Object[] { applicationProviderId, context.getProperties().get(ComponentConstants.COMPONENT_NAME), context.getBundleContext().getBundle() });
+			setId(applicationProviderId);
+		} catch (final IllegalStateException e) {
+			// compare and only continue if match
+			if (!applicationProviderId.equals(getId()))
+				throw new IllegalStateException(String.format("The StaticResourceApplicationProvider has already been initialized with an application provider id (%s) and cannot be initialized again with a different id (%s). Please check your component configuration!", getId(), applicationProviderId), e);
+		}
+
+		// remember bundle for later use
+		bundleContext = context.getBundleContext();
+
+		// remember resource path
+		bundleResourcePath = (String) context.getProperties().get("resource.path");
+		devModeDocRootEnvVar = (String) context.getProperties().get("resource.devModeDocRootEnvVar");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.gyrex.http.application.provider.ApplicationProvider#createApplication(java.lang.String, org.eclipse.gyrex.context.IRuntimeContext)
+	 */
+	@Override
+	public Application createApplication(final String applicationId, final IRuntimeContext context) throws Exception {
+		return new StaticResourceApplication(applicationId, context, bundleContext.getBundle(), bundleResourcePath, devModeDocRootEnvVar);
+	}
+
+	public void deactivate(final ComponentContext context) {
+		LOG.debug("StaticResourceApplicationProvider de-activation triggered for component '{}' (bundle {})", context.getProperties().get(ComponentConstants.COMPONENT_NAME), context.getBundleContext().getBundle());
+		bundleContext = null;
+	}
+
+	private String getApplicationProviderId(final ComponentContext context) {
+		final Object applicationProviderIdValue = context.getProperties().get(APPLICATION_PROVIDER_ID);
+
+		if (null == applicationProviderIdValue)
+			return (String) context.getProperties().get(ComponentConstants.COMPONENT_NAME);
+
+		if (!(applicationProviderIdValue instanceof String))
+			throw new IllegalStateException("The StaticResourceApplicationProvider property 'applicationProviderId' must be of type String!");
+		return (String) applicationProviderIdValue;
+	}
+
+}
