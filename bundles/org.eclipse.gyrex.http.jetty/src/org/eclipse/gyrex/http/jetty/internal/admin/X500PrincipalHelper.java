bf479950b06ac1ac36de0d7c7ea439b8998babf7
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2013-03-17 17:26:25 +0100

    update copyright headers
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- bundles/org.eclipse.gyrex.http.jetty/src/org/eclipse/gyrex/http/jetty/internal/admin/X500PrincipalHelper.java
+++ bundles/org.eclipse.gyrex.http.jetty/src/org/eclipse/gyrex/http/jetty/internal/admin/X500PrincipalHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Jay Rosenthal, AGETO Service GmbH and others.
+ * Copyright (c) 2008, 2012 Jay Rosenthal, AGETO Service GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
